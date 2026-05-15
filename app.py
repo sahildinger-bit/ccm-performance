eb5a92f81d208c8ddc56afca5e99ac8508a0f4ab
sahildinger-bit <sa.hildinger@gmail.com>
2026-05-15 13:56:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -870,9 +870,7 @@ write(330, 410, f"{netto:.2f} Euro")
 write(330, 435, f"{mwst:.2f} Euro")
 write(330, 465, f"{brutto:.2f} Euro")
 
-write(160, 735, invoice_no)
-write(360, 735, contract_no)
-write(500, 735, invoice_date)
+
 
 doc.save(output)
 return send_file(output, as_attachment=False)
