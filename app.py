cdf8cbe493ed372946bb49a2ba1cdcdc8574f007
sahildinger-bit <sa.hildinger@gmail.com>
2026-05-20 18:28:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -30,21 +30,20 @@ def get_db():
         g.db = psycopg2.connect(DATABASE_URL)
     return g.db
 
+try:
+    cur.execute("ALTER TABLE vehicles ADD COLUMN image_link TEXT")
+except:
+    pass
 
+try:
+    cur.execute("ALTER TABLE vehicles ADD COLUMN color_tag TEXT DEFAULT ''")
+except:
+    pass
 
-def init_db():
-    db = psycopg2.connect(os.environ.get("DATABASE_URL"))
-    cur = db.cursor()
-
-    try:
-        cur.execute("ALTER TABLE bookings ADD COLUMN vehicle_id INTEGER")
-    except:
-        pass
-
-    try:
-        cur.execute("ALTER TABLE bookings ADD COLUMN customer_id INTEGER")
-    except:
-        pass
+try:
+    cur.execute("ALTER TABLE vehicles ADD COLUMN note TEXT DEFAULT ''")
+except:
+    pass
 
     cur.execute("""
     CREATE TABLE IF NOT EXISTS vehicles (
