75479c4de8d9571ca5932a01a2b91b3bb1c47656
sahildinger-bit <sa.hildinger@gmail.com>
2026-05-20 18:14:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -97,13 +97,14 @@ def init_db():
     cur = db.cursor()
 
     cur.execute("""CREATE TABLE IF NOT EXISTS users(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        id SERIAL PRIMARY KEY 
         username TEXT UNIQUE NOT NULL,
         password TEXT NOT NULL
     )""")
 
-    cur.execute("""CREATE TABLE IF NOT EXISTS vehicles(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS vehicles(
+        id SERIAL PRIMARY KEY 
         name TEXT NOT NULL,
         plate TEXT NOT NULL,
         daily_price REAL NOT NULL,
@@ -122,7 +123,7 @@ def init_db():
     )""")
 
     cur.execute("""CREATE TABLE IF NOT EXISTS customers(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        id SERIAL PRIMARY KEY 
         first_name TEXT NOT NULL,
         last_name TEXT NOT NULL,
         phone TEXT,
@@ -136,8 +137,9 @@ def init_db():
         note TEXT
     )""")
 
-    cur.execute("""CREATE TABLE IF NOT EXISTS bookings(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS bookings(
+        id SERIAL PRIMARY KEY 
         vehicle_id INTEGER NOT NULL,
         customer_id INTEGER NOT NULL,
         start_date TEXT NOT NULL,
@@ -158,8 +160,9 @@ def init_db():
         FOREIGN KEY(customer_id) REFERENCES customers(id)
     )""")
 
-    cur.execute("""CREATE TABLE IF NOT EXISTS reservations(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS reservations(
+        id SERIAL PRIMARY KEY,
         customer_name TEXT NOT NULL,
         phone TEXT,
         email TEXT,
@@ -174,9 +177,9 @@ def init_db():
         created_at TEXT DEFAULT CURRENT_TIMESTAMP
     )""")
 
-    cur.execute("""CREATE TABLE IF NOT EXISTS damages(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        report_date TEXT NOT NULL,
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS damages(
+        id SERIAL PRIMARY KEY 
         vehicle_id INTEGER NOT NULL,
         customer_id INTEGER,
         description TEXT NOT NULL,
@@ -189,14 +192,17 @@ def init_db():
         FOREIGN KEY(customer_id) REFERENCES customers(id)
     )""")
 
-    cur.execute("""CREATE TABLE IF NOT EXISTS settings(
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS settings(
         key TEXT PRIMARY KEY,
         value TEXT NOT NULL
     )""")
 
     cur.execute("SELECT COUNT(*) FROM users")
     if cur.fetchone()[0] == 0:
-        cur.execute("INSERT INTO users(username,password) VALUES (?,?)", ("admin", "ccm123"))
+        cur.execute(
+            "INSERT INTO users(username,password) VALUES (%,%)", 
+            ("admin", "ccm123"))
 
     cur.execute("SELECT COUNT(*) FROM vehicles")
     if cur.fetchone()[0] == 0:
